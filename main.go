c4f8daff667750d4b447325cb81cb3bb6321b6b8
gfermini <gfermini@gmail.com>
2023-01-04 19:43:53 +0300

    añadido carácter especial – (U+2013)

--- main.go
+++ main.go
@@ -126,7 +126,7 @@ func main() {
 
 func logica(pantallaBinding binding.String, chances binding.String, link *widget.Hyperlink, input *widgets.EntradaConEvento, botonE *widget.Button, channel chan string) {
 	palabra := scrap.PrintPalabra()
-	var letras string = " ()-,.áàäãâæéèëíìïóòöøúùüßšçπ0123456789€%$&#'\"\\/[]{}!¡¿?+=:_<>·@#|°®"
+	var letras string = " ()-,–.áàäãâæéèëíìïóòöøúùüßšçπ0123456789€%$&#'\"\\/[]{}!¡¿?+=:_<>·@#|°®"
 	if link.Visible() {
 		link.Hide()
 	}
